e511c0f67284452882e5591ab557ddead1eeeef0
Ryosukekamimura <come1234560@gmail.com>
2021-02-02 12:45:08 +0900

    feat: Create api

--- myapp/backend/app.js
+++ myapp/backend/app.js
@@ -7,7 +7,6 @@ var mongoose = require('mongoose')
 
 // routers
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var coursesRouter = require('./routes/courses');
 
 // express instance
@@ -32,8 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // setting router
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
+
+// course list
+app.use('/api/v1/courses', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/backend/routes/index.js
+++ myapp/backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/courses')
+  res.redirect('/api/v1/courses')
 });
 
 module.exports = router;
