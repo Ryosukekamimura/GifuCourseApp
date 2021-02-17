ba01bf14ec35502d80e91c7116d6425461bbbbaa
Ryosukekamimura <come1234560@gmail.com>
2021-02-17 09:19:38 +0900

    fix: Fix Typos

--- myapp/frontend/pages/latterCourse/_id.vue
+++ myapp/frontend/pages/latterCourse/_id.vue
@@ -6,7 +6,7 @@
 
     <v-card color="blue lighten-4">
 
-      <div v-if="isFetched" align="center">
+      <div v-if="isCourseFetched" align="center">
         <div class="ma-4">
           <h3>講義名</h3>
           <h1>
@@ -35,102 +35,120 @@
         </div>
       </div>
 
-        <v-spacer></v-spacer>
-      </v-card>
       <v-spacer></v-spacer>
+    </v-card>
+    <v-spacer></v-spacer>
 
 
-      <div align="left">
-        <h3>コメント一覧</h3>
-        <h5 align="center">コメントはありません</h5>
+    <div align="center">
+      <h3>みんなのコメント</h3>
+      <!-- コメント一覧を取得する -->
+      <div v-if="isCommentFetched">
+        <li v-for="(comment, index) in commentData" :key="index">
+          {{ comment.comment }}
+        </li>
       </div>
-      <v-spacer></v-spacer>
-
-      <v-textarea
-        label="感想 [ex: 面白かった] [ex: 先生がずっと話している講義だった] [ex: 〇〇を改善してほしい]"
-        auto-grow
-        outlined
-        rows="1"
-        row-height="15"
-        class="ma-4"
-      ></v-textarea>
-      <div align="center">
-        <p>個人を特定したコメント並びに、誹謗中傷は禁止させていただきます。</p>
-        <p>感想を送信するには、ログインが必要となります。</p>
-        <v-btn depressed color="blue" @click="postComments('Nice Course!', course)">
-          送信
-        </v-btn>
-        <p>{{ checkLoginMessage }}</p>
+      <div v-else>
+        <h3>コメントはありません。</h3>
       </div>
+    </div>
+    <v-spacer></v-spacer>
 
-      <div>
-        <v-btn @click="fetchComments">
-          コメントを確認する
-        </v-btn>
-      </div>
+    <v-textarea
+      v-model = 'message'
+      label="感想 [ex: 面白かった] [ex: 先生がずっと話している講義だった] [ex: 〇〇を改善してほしい]"
+      auto-grow
+      outlined
+      rows="1"
+      row-height="15"
+      class="ma-4"
+    ></v-textarea>
+    <div align="center">
+      <p>個人を特定したコメント並びに、誹謗中傷は禁止させていただきます。</p>
+      <p>感想を送信するには、ログインが必要となります。</p>
+      <v-btn depressed color="blue" @click="postComments(message, course)">
+        送信
+      </v-btn>
+      <p>{{ checkLoginMessage }}</p>
+      <h2>{{ statusMessage }}</h2>
+    </div>
+
+
   </v-container>
 </template>
 
 <script>
-  import axios from "axios";
-  export default {
-    data() {
-      return {
-        course: null,
-        isFetched: false,
-        checkLoginMessage: "",
-        comments: null,
+import axios from "axios";
+export default {
+  data() {
+    return {
+      message: "",
+      course: null,
+      isCourseFetched: false,
+      checkLoginMessage: "",
+      commentData: null,
+      isCommentFetched: false,
+      statusMessage: "",
+    }
+  },
+  methods: {
+    // シラバスのURLを作成
+    getCourseURL: function(id) {
+      return "https://alss-portal.gifu-u.ac.jp/campusweb/slbssbdr.do?value(risyunen)=2020&value(semekikn)=1&value(kougicd)=" + id + "&value(crclumcd)="
+    },
+    // ログインしているかチェックする
+    checkLogin: function(){
+      if (this.$store.state.isLogin){
+        this.checkLoginMessage = '🤩ログインしています！'
+      }else{
+        this.checkLoginMessage = 'ログインしていません！'
       }
     },
-    methods: {
-      // シラバスのURLを作成
-      getCourseURL: function(id) {
-        return "https://alss-portal.gifu-u.ac.jp/campusweb/slbssbdr.do?value(risyunen)=2020&value(semekikn)=1&value(kougicd)=" + id + "&value(crclumcd)="
-      },
-      // ログインしているかチェックする
-      checkLogin: function(){
-        if (this.$store.state.isLogin){
-          this.checkLoginMessage = '🤩ログインしています！'
-        }else{
-          this.checkLoginMessage = 'ログインしていません！'
-        }
-      },
-      // コメントをpostする
-      postComments: function(comment ,course){
+    // コメントをpostする
+    postComments: function(comment ,course){
+      if (comment == ""){
+        this.statusMessage = "コメントを入力してください🥺"
+        setTimeout(this.brankStatusMessage, 3000)
+      }else if (!this.$store.state.isLogin) {
+        this.statusMessage = "ログインしてください👀"
+        setTimeout(this.brankStatusMessage, 3000)
+      }else{
+        let poster = this.$store.state.displayName
         axios.post("http://localhost:8000/api/v1/courses/create/comments", {
-          id: course._id,
-          comment: comment
-        }, {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer"
-          }
+          course_id: course._id,
+          comment: comment,
+          poster: poster,
+          serverTimeStamp: Date.now(),
         })
-        .then(() => {
-          console.log("Success To Send")
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      },
-      // コメントをFetchする
-      fetchComments: function(){
-        axios.get('http://localhost:8000/api/v1/courses/601d1dc76f30f41c7072c5ae/comments')
-        .then(res => {
-          console.log('Fetch コメント')
-          console.log(res.data)
-        })
+        this.statusMessage = "Thank you! コメントを投稿できました！🎉"
+        // statusMessageを空白にする
+        setTimeout(this.brankStatusMessage, 3000)
+        // 入力コメントをnullにする
+        this.message = ""
       }
     },
-    mounted() {
-      // HTTP request
-      axios.get('http://localhost:8000/api/v1/courses/latter/' + this.$route.params.id )
+    //statusMessageを空白にする
+    brankStatusMessage: function(){
+      this.statusMessage = ""
+    },
+  },
+  mounted() {
+    // HTTP request
+    axios.get('http://localhost:8000/api/v1/courses/latter/' + this.$route.params.id )
       .then(res => {
         console.log('Success to Fetch API')
         console.log(res.data)
         this.course = res.data
-        this.isFetched = true
-      })
-    }
+        this.isCourseFetched = true
+      }),
+      axios.get('http://localhost:8000/api/v1/courses/' + this.$route.params.id + '/comments')
+        .then(res => {
+          console.log('Fetch コメント')
+          console.log(res.data)
+          this.commentData = res.data
+          console.log(this.commentData)
+          this.isCommentFetched = true
+        })
   }
+}
 </script>
